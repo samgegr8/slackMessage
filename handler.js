1d9ee6b076d616a2a177e064d4fc331b11322ddb
samratVF <samrat.nitrkl@gmail.com>
2019-05-20 15:22:38 +1000

    Changes on the Code

--- handler.js
+++ handler.js
@@ -1,14 +1,44 @@
-'use strict';
+ 
+var Slack = require('slack-node');
+
+var webhookUri = "https://hooks.slack.com/services/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";
+
+var slack = new Slack();
+ 
+var res;
 
 module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    console.log("Nice To Know");
+    res = await sendSlackMessage(webhookUri);
+    console.log(res);
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: res,
+            input: event,
+        }, null, 2),
+    };
 };
+
+const sendSlackMessage = (webhookUri) => {
+    return new Promise((resolve, reject) => {
+        message = "Testing Message to Slack Channel";
+        slack.setWebhook(webhookUri);
+        slack.webhook({
+            channel: "#general",
+            username: "Samrat",
+            icon_emoji: ":ghost:",
+            text: message
+        }, function (err, response) {
+            if (err) {
+                console.log(err); // an error occurred
+                reject(err);
+            } else {
+                resolve(response);
+                console.log(response);
+            }
+        });
+    });
+};
+
+
